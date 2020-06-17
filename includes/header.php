b44c558b6ccee5a5af0f8498a2d70e7ffd3d7c0b
Tejas310895 <tshirsat700@gmail.com>
2020-06-17 22:23:23 +0530

    styles

--- includes/header.php
+++ includes/header.php
@@ -81,7 +81,7 @@ if(isset($_GET['pro_id'])){
      <script src="js/script.js"></script>
     <!-- date -->
     <!-- styles -->
-    <link rel="stylesheet" href="styles/style.css?version=6">
+    <link rel="stylesheet" href="styles/style.css?version=7">
     <!-- styles -->
 </head>
 <body>
