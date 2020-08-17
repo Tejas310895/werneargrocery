c783edf98ea4124ee6a6cf0fb3212fe429763c95
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-08-17 17:41:24 +0530

    error

--- cart.php
+++ cart.php
@@ -205,7 +205,7 @@
                     echo "
                     
                     <a class='btn btn-success pull-right bill_checkout' style='color:#fff;'>
-                    Add Rs.$required More
+                    Add ₹$required More
                     </a>
                     
                     ";
